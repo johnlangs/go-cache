dd3ada220e0ec8409c2c2fb8c418cc13ade10120
John Langs <me@johnlangs.dev>
2024-01-17 10:54:58 -0600

    struct test

--- cache_test.go
+++ cache_test.go
@@ -1,6 +1,9 @@
 package cache
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSet(t *testing.T) {
 	c := CreateCache(0)
@@ -58,6 +61,45 @@ func TestDelete(t *testing.T) {
 
 	c.Delete("A")
 	result, ok = c.Get("A")
+	if result != nil {
+		t.Errorf("Failed to delete value: Not nil")
+	}
+	if ok {
+		t.Errorf("Failed to delete value: Access was Ok")
+	}
+}
+
+func TestStructStorage(t *testing.T) {
+	type thisStruct struct {
+		num int
+		str string
+		nums []int
+	}
+
+	someObj := thisStruct{10, "Hello!", []int{1, 2, 3}}
+	c := CreateCache(0)
+
+	err := c.Set("struct", someObj)
+	if err != nil {
+		t.Errorf("Failed to Set cache value")
+	}
+
+	var result interface{}
+	result, ok := c.Get("struct")
+	if !ok {
+		t.Errorf("Failed to Get cache value: Not Ok on Get")
+	}
+
+	resultStruct, ok := result.(thisStruct)
+	if !ok {
+		t.Errorf("Failed to Get cache value: Not Ok on type conversion")
+	}
+	if (resultStruct.num != 10) || (resultStruct.str != "Hello!") || (!slices.Equal(resultStruct.nums, []int{1, 2, 3})) {
+		t.Errorf("Failed to Get cache value: Item not equal to original inserted value")
+	}
+
+	c.Delete("struct")
+	result, ok = c.Get("struct")
 	if result != nil {
 		t.Errorf("Failed to delete value: Not nil")
 	}
